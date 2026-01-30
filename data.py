8d7b154f9ff4396d5414758b427c9b1eacfee072
kikoplaystar-droid <kikoplaystar@gmail.com>
2026-01-30 17:34:25 +0100

    Update data.py

--- data.py
+++ data.py
@@ -16,15 +16,17 @@ def get_all_movies(
 
 # -------------------- Get Movies By Username --------------------
 def get_movies(
-    username: str,
+    username: str | None = None,
     file_path: str = "data.json",
     movie_id: int | None = None
-) -> list[dict] | dict:
-    """Returns a user's movie list or a single movie from file_path."""
+):
     with open(file_path, 'r') as fp:
         movies = json.load(fp)
 
-        if username not in movies.keys():
+        if username is None:
+            return movies
+
+        if username not in movies:
             return []
 
         if movie_id is not None and movie_id < len(movies[username]):
@@ -33,16 +35,21 @@ def get_movies(
         return movies[username]
 
 
+
 # -------------------- Add Movie --------------------
 def add_movie(
+    username: str,
     movie: dict,
-    file_path: str = "movies.json",
+    file_path: str = "data.json",
 ):
     """Adds a movie to file_path."""
     movies = get_all_movies(file_path=file_path, movie_id=None)
 
     if movies is not None:
-        movies.append(movie)
+        if username not in movies.keys():
+            movies[username] = []
+
+        movies[username].append(movie)
         with open(file_path, "w") as fp:
             json.dump(
                 movies,
@@ -54,14 +61,16 @@ def add_movie(
 
 # -------------------- Delete Movie --------------------
 def delete_movie(
+    username: str,
     movie_to_delete: dict,
-    file_path: str = "movies.json",
+    file_path: str = "data.json",
 ) -> None:
     """Deletes a movie from file_path by name."""
     movies = get_all_movies(file_path=file_path, movie_id=None)
 
-    movies = [movie for movie in movies if movie["name"]
+    user_movies = [movie for movie in movies[username] if movie["name"]
               != movie_to_delete["name"]]
+    movies[username] = user_movies
 
     with open(file_path, "w") as fp:
         json.dump(
@@ -70,19 +79,21 @@ def delete_movie(
             indent=4,
             ensure_ascii=False,
         )
+    return True
 
 
 # -------------------- Edit Movie --------------------
 def edit_movie(
+        username: str,
     movie_to_edit: dict,
-    file_path: str = "movies.json",
+    file_path: str = "data.json",
 ) -> None:
     """Updates a movie description in file_path by name."""
     movies = get_all_movies(file_path=file_path, movie_id=None)
 
-    for i, movie in enumerate(movies):
+    for i, movie in enumerate(movies[username]):
         if movie_to_edit["name"] == movie["name"]:
-            movies[i] = movie_to_edit
+            movies[username][i] = movie_to_edit
 
     with open(file_path, "w") as fp:
         json.dump(
