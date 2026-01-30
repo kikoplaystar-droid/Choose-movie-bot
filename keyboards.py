17b39e50e26ce4e4b3e6bda47d7a46ec2bd924d1
kikoplaystar-droid <kikoplaystar@gmail.com>
2026-01-30 17:35:07 +0100

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,36 +1,13 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.filters.callback_data import CallbackData
+import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
 
 
-class MovieCallback(CallbackData, prefix="movie", sep=";"):
-    id: int
-    name: str
-
-
-def movies_keyboard_markup(
-    movies_list: list[dict],
-    offset: int | None = None,
-    skip: int | None = None
-):
-    """
-    Creates an inline keyboard based on the provided list of movies.
-
-    Example:
-    >>> await message.answer(
-            text="Some text",
-            reply_markup=movies_keyboard_markup(movies_list)
-        )
-    """
-
-    builder = InlineKeyboardBuilder()
-    builder.adjust(1, repeat=True)
-
-    for index, movie_data in enumerate(movies_list):
-        callback_data = MovieCallback(id=index, **movie_data)
-        builder.button(
-            text=callback_data.name,
-            callback_data=callback_data.pack()
-        )
-
-    builder.adjust(1, repeat=True)
-    return builder.as_markup()
+def save_image(url: str) -> str:
+    """Downloads an image from a URL and saves it locally."""
+    response = requests.get(url, timeout=15, headers=headers)
+    with open("file.jpg", "wb") as file:
+        file.write(response.content)
+    return "file.jpg"
